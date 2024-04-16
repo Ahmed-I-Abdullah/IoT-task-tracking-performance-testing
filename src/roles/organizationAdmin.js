5f3eae895f4c194f54869ac0698c36d8a36e08c2
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-04-15 22:27:13 -0600

    feat: finish org admin

--- src/roles/organizationAdmin.js
+++ src/roles/organizationAdmin.js
@@ -1,21 +1,31 @@
 import { describe, expect } from "../util/chaiExpect.js";
 import {
-  createOrganization,
-  getOrganization,
-  updateOrganization,
-  deleteOrganization,
-  getAllOrganizations,
-} from "../models/organization.js";
-import { login, registerUser } from "../util/authentication.js";
+  createClinic,
+  deleteClinic,
+  getClinic,
+  getOrganizationClinics,
+  updateClinic,
+} from "../models/clinic.js";
 import {
-  createOrganizationAdmin,
-  createSuperAdmin,
-  deleteOrganizationAdmin,
-} from "../models/user.js";
-import { createClinic, deleteClinic } from "../models/clinic.js";
+  createOrganizationTask,
+  deleteOrganizationTask,
+  getOrganizationTasks,
+} from "../models/task.js";
+import { createClinicAdmin, deleteClinicAdmin, getClinicAdmins } from "../models/user.js";
+import { login, confirmRegister } from "../util/authentication.js";
+
+const CLINIC_ADMIN_INFO = {
+  email: "clinicAdmin@test.com",
+  password: "veryVerySecure",
+  firstName: "Clinic",
+  middleName: "Admin",
+  lastName: "Test",
+};
+
 
 export function organizationAdminOperations(authToken, organizationId) {
   let clinicId = "";
+  let orgTaskId = "";
   let clinicAdminId = "";
 
   const CLINIC_PAYLOAD = {
@@ -31,7 +41,82 @@ export function organizationAdminOperations(authToken, organizationId) {
     clinicId = createClinic(authToken, organizationId, CLINIC_PAYLOAD);
   });
 
+  describe("Get the created clinic", () => {
+    getClinic(authToken, organizationId, clinicId);
+  });
+
+  describe("Update the clinic", () => {
+    const UPDATED_PAYLOAD = {
+      name: `Updated Clinic Name`,
+    };
+    updateClinic(authToken, organizationId, clinicId, UPDATED_PAYLOAD);
+  });
+
+  describe("Get all clinics in organization", () => {
+    getOrganizationClinics(authToken, organizationId);
+  });
+
+  describe("Create organization task", () => {
+    const ORG_TASK_PAYLOAD = {
+      name: "Hand Washing",
+      description: "This is a nice description!",
+    };
+    orgTaskId = createOrganizationTask(
+      authToken,
+      organizationId,
+      ORG_TASK_PAYLOAD
+    );
+  });
+
+  describe("List organization tasks", () => {
+    getOrganizationTasks(authToken, organizationId);
+  });
+
+  describe("Delete created organization task", () => {
+    deleteOrganizationTask(authToken, organizationId, orgTaskId);
+  });
+
+
+  describe("Create clinic admin", () => {
+    const { userId, registerationToken, isRegistered } =
+      createClinicAdmin(authToken, CLINIC_ADMIN_INFO.email, organizationId, clinicId)
+    clinicAdminId = userId;
+
+    if (!isRegistered) {
+      describe("Register organization admin", () => {
+        confirmRegister(
+          registerationToken,
+          CLINIC_ADMIN_INFO.firstName,
+          CLINIC_ADMIN_INFO.middleName,
+          CLINIC_ADMIN_INFO.lastName,
+          CLINIC_ADMIN_INFO.password
+        );
+      });
+    }
+
+    // Login the organization admin
+    describe("Login clinic admin", () => {
+      const orgAdminAuthToken = login(
+        CLINIC_ADMIN_INFO.email,
+        CLINIC_ADMIN_INFO.password
+      );
+
+      // TODO: 
+     // Call clinic admin scripts here
+    });
+  });
+
+  describe(`Get clinic admins for clinic ${clinicId}`, () => {
+    getClinicAdmins(authToken, organizationId, clinicId);
+  });
+
+
+  describe("Delete clinic admin", () => {
+    deleteClinicAdmin(authToken, organizationId, clinicId, clinicAdminId);
+  });
+
+
   describe(`Delete the clinic ${clinicId}`, () => {
-    deleteClinic(authToken, organizationId, clinicId)
+    deleteClinic(authToken, organizationId, clinicId);
   });
 }
