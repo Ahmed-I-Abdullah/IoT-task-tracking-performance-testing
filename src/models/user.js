739d1d00c885d97567223805b86dd26707f50711
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-04-15 20:23:22 -0600

    feat: finish super admin tests

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,16 @@
 import { session } from "../util/session.js";
 import { describe, expect } from "../util/chaiExpect.js";
+import { isValidUUID } from "../util/uuidCheck.js";
 
 /**
  * Creates a user with the provided email.
+ * @param {string} authToken - The authentication token.
  * @param {string} endpoint - The API endpoint for user creation.
  * @param {string} email - The email of the user to be created.
- * @returns {Object} An object containing the registration token and registration status.
+ * @returns {Object} An object containing the userId, registration token, and registration status.
  */
-export function createUser(endpoint, email) {
+export function createUser(authToken, endpoint, email) {
+  session.addHeader("Authorization", `Bearer ${authToken}`);
   const resp = session.post(
     endpoint,
     JSON.stringify({
@@ -17,36 +20,70 @@ export function createUser(endpoint, email) {
 
   // Ensure that the user creation was successful (HTTP status 201)
   expect(resp.status, "User creation status").to.equal(201);
-
-  // Ensure that the response body is a valid JSON
   expect(resp, "User creation valid JSON response").to.have.validJsonBody();
 
   // Ensure that the registration token is a string
-  const registerationToken = resp.json("data.registrationToken");
+  const registerationToken = resp.json("data.registrationToken") || resp.json("data.user.registrationToken");
   expect(registerationToken, "Registration token").to.be.a("string");
 
   // Ensure that the registration status is a boolean
-  const isRegistered = resp.json("data.isRegistered");
+  const isRegistered = resp.json("data.isRegistered") || resp.json("data.user.isRegistered");
   expect(isRegistered, "isRegistered").to.be.a("boolean");
 
-  return { registerationToken, isRegistered };
+  // Ensure that the user ID is a valid UUID
+  const userId = resp.json("data.id") || resp.json("data.user.id");
+  expect(isValidUUID(userId), "Valid user UUID").to.be.true;
+
+  return { userId, registerationToken, isRegistered };
+}
+
+/**
+ * Deletes a user.
+ * @param {string} authToken - The authentication token.
+ * @param {string} endpoint - The API endpoint for user deletion.
+ */
+export function deleteUser(authToken, endpoint) {
+  session.addHeader("Authorization", `Bearer ${authToken}`);
+  const resp = session.delete(endpoint);
+
+  // Ensure that the user deletion was successful (HTTP status 204)
+  expect(resp.status, "User deletion status").to.equal(204);
 }
 
 /**
  * Creates a super admin with the provided email.
+ * @param {string} authToken - The authentication token.
  * @param {string} email - The email of the super admin to be created.
- * @returns {Object} An object containing the registration token and registration status.
+ * @returns {Object} An object containing the userId, registration token, and registration status.
  */
-export function createSuperAdmin(email) {
-  return createUser("/api/v1/super_admins", email);
+export function createSuperAdmin(authToken, email) {
+  return createUser(authToken, "/api/v1/super_admins", email);
 }
 
 /**
  * Creates an organization admin with the provided email and organization ID.
+ * @param {string} authToken - The authentication token.
  * @param {string} email - The email of the organization admin to be created.
  * @param {string} organizationId - The ID of the organization.
- * @returns {Object} An object containing the registration token and registration status.
+ * @returns {Object} An object containing the userId, registration token, and registration status.
  */
-export function createOrganizationAdmin(email, organizationId) {
-  return createUser(`/api/v1/organizations/${organizationId}/admins`, email);
+export function createOrganizationAdmin(authToken, email, organizationId) {
+  return createUser(
+    authToken,
+    `/api/v1/organizations/${organizationId}/admins`,
+    email
+  );
+}
+
+/**
+ * Deletes an organization admin with the provided authentication token, organization ID, and user ID.
+ * @param {string} authToken - The authentication token.
+ * @param {string} organizationId - The ID of the organization.
+ * @param {string} userId - The ID of the user to be deleted.
+ */
+export function deleteOrganizationAdmin(authToken, organizationId, userId) {
+  return deleteUser(
+    authToken,
+    `/api/v1/organizations/${organizationId}/admins/${userId}`
+  );
 }
--- src/roles/superAdmin.js
+++ src/roles/superAdmin.js
@@ -1,72 +1,109 @@
-import { describe, expect } from '../util/chaiExpect.js'
-import { createOrganization, getOrganization, updateOrganization, deleteOrganization } from '../models/organization.js'
-import { login, registerUser } from '../util/authentication.js'
-import { createSuperAdmin } from '../models/user.js'
+import { describe, expect } from "../util/chaiExpect.js";
+import {
+  createOrganization,
+  getOrganization,
+  updateOrganization,
+  deleteOrganization,
+  getAllOrganizations,
+} from "../models/organization.js";
+import { login, registerUser } from "../util/authentication.js";
+import { createOrganizationAdmin, createSuperAdmin, deleteOrganizationAdmin } from "../models/user.js";
 
 // Constants for super admin registration
-const EMAIL = "superadmin@test.com"
-const PASSWORD = "verySecure"
-const USER_FIRST_NAME = "Super"
-const USER_MIDDLE_NAME = "Admin"
-const USER_LAST_NAME = "Test"
+const EMAIL = "superadmin@test.com";
+const PASSWORD = "verySecure";
+const USER_FIRST_NAME = "Super";
+const USER_MIDDLE_NAME = "Admin";
+const USER_LAST_NAME = "Test";
 
 // Function to set up the environment
 export function setup() {
-    let authToken = '';
+  let authToken = "";
 
-    // Set up the super admin user
-    describe(`Setup - Create and authenticate super admin ${EMAIL}`, () => {
-        // Create super admin if not already registered
-        const {registerationToken, isRegistered} = createSuperAdmin(EMAIL);
-        if (!isRegistered) {
-            describe('Register a new User', () => {
-                registerUser(registerationToken, USER_FIRST_NAME, USER_MIDDLE_NAME, USER_LAST_NAME, PASSWORD)
-            })
-        }
+  // Set up the super admin user
+  describe(`Setup - Create and authenticate super admin ${EMAIL}`, () => {
+    // Create super admin if not already registered
+    const { registerationToken, isRegistered } = createSuperAdmin(null, EMAIL);
+    if (!isRegistered) {
+      describe("Register a new User", () => {
+        registerUser(
+          registerationToken,
+          USER_FIRST_NAME,
+          USER_MIDDLE_NAME,
+          USER_LAST_NAME,
+          PASSWORD
+        );
+      });
+    }
 
-        // Login the super admin user
-        describe('Login User', () => {
-            authToken = login(EMAIL, PASSWORD)
-        });
-    })
+    // Login the super admin user
+    describe("Login User", () => {
+      authToken = login(EMAIL, PASSWORD);
+    });
+  });
 
-    return authToken
+  return authToken;
 }
 
 // Function to perform super admin operations
 export default function superAdminOperations(authToken) {
-    describe('Super admin CRUD operations on organizations', () => {
-        const ORGANIZATION_PAYLOAD = {
-            name: "Ahmed's Corp",
-            streetAddress: '1564 Burdett Avenue',
-            city: 'Terrace',
-            province: 'British Columbia',
-            country: 'Canada',
-            postalCode: 'V8G 1S2',
-          }
-        let organizationId = "";
+  describe("Super admin CRUD operations on organizations", () => {
+    let organizationId = "";
+    let orgAdminId = "";
 
-        // Create a new organization
-        describe('Create a new organization', () => {
-            organizationId = createOrganization(authToken, ORGANIZATION_PAYLOAD)
-        })
+    const ORGANIZATION_PAYLOAD = {
+      name: "Ahmed's Corp",
+      streetAddress: "1564 Burdett Avenue",
+      city: "Terrace",
+      province: "British Columbia",
+      country: "Canada",
+      postalCode: "V8G 1S2",
+    };
 
-        // Get the created organization
-        describe('Get the created organization', () => {
-            getOrganization(authToken, organizationId)
-        })
+    // Create a new organization
+    describe("Create a new organization", () => {
+      organizationId = createOrganization(authToken, ORGANIZATION_PAYLOAD);
+    });
 
-        // Update the organization
-        describe('Update the organization', () => {
-            const UPDATED_PAYLOAD = {
-                name: `Updated Organization Name`,
-              }
-            updateOrganization(authToken, organizationId, UPDATED_PAYLOAD)
-        })
+    // Get the created organization
+    describe("Get the created organization", () => {
+      getOrganization(authToken, organizationId);
+    });
 
-        // Delete the organization
-        describe('Delete the organization', () => {
-            deleteOrganization(authToken, organizationId)
-        })
-    })
+    // Update the organization
+    describe("Update the organization", () => {
+      const UPDATED_PAYLOAD = {
+        name: `Updated Organization Name`,
+      };
+      updateOrganization(authToken, organizationId, UPDATED_PAYLOAD);
+    });
+
+    // List all available organizations
+    describe("Get all organizations", () => {
+      getAllOrganizations(authToken);
+    });
+
+
+    // Create an admin for the organization
+    describe("Create organization admin", () => {
+      const { userId } = createOrganizationAdmin(
+        authToken,
+        "orgAdmin@test.com",
+        organizationId
+      );
+      orgAdminId = userId;
+    });
+
+
+    // Delete the organization admin
+    describe("Delete organization admin", () => {
+        deleteOrganizationAdmin(authToken, organizationId, orgAdminId);
+      });
+
+
+    // Delete the organization
+    describe("Delete the organization", () => {
+      deleteOrganization(authToken, organizationId);
+    });
+  });
 }
