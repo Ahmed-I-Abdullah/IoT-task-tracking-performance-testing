f2a5f2af7e3d3e616559c250980248d1d0894a54
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-04-15 19:15:09 -0600

    documentation: add docs

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,13 @@
 import { session } from "../util/session.js";
 import { describe, expect } from '../util/chaiExpect.js';
 
+/**
+ * Creates a super admin with the provided email.
+ * @param {string} email - The email of the super admin to be created.
+ * @returns {Object} An object containing the registration token and registration status.
+ */
 export function createSuperAdmin(email) {
+  // Send a POST request to create a super admin
   const resp = session.post(
     `/api/v1/super_admins`,
     JSON.stringify({
@@ -9,12 +15,16 @@ export function createSuperAdmin(email) {
     })
   );
 
+  // Ensure that the user creation was successful (HTTP status 201)
   expect(resp.status, "User creation status").to.equal(201);
+  // Ensure that the response body is a valid JSON
   expect(resp, "User creation valid json response").to.have.validJsonBody();
 
+  // Ensure that the registration token is a string
   const registerationToken = resp.json("data.registrationToken");
   expect(registerationToken, "Registration token").to.be.a("string");
 
+  //  Ensure that the registration status is a boolean
   const isRegistered = resp.json("data.isRegistered");
   expect(isRegistered, "isRegistered").to.be.a("boolean");
 
--- src/util/uuidCheck.js
+++ src/util/uuidCheck.js
@@ -1,6 +1,11 @@
 const uuidRegex =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+  /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
+/**
+ * Validates a UUID using a regular expression.
+ * @param {string} uuid - The UUID to validate.
+ * @returns {boolean} True if the UUID is valid, otherwise false.
+ */
 export function isValidUUID(uuid) {
-  return uuidRegex.test(uuid)
+  return uuidRegex.test(uuid);
 }
