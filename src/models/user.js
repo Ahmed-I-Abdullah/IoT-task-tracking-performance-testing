ac04c4f07d8f36621a6ea5d928b2a9a26b475762
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-04-15 19:54:46 -0600

    refactor: better way for creating users

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,15 @@
 import { session } from "../util/session.js";
-import { describe, expect } from '../util/chaiExpect.js';
+import { describe, expect } from "../util/chaiExpect.js";
 
 /**
- * Creates a super admin with the provided email.
- * @param {string} email - The email of the super admin to be created.
+ * Creates a user with the provided email.
+ * @param {string} endpoint - The API endpoint for user creation.
+ * @param {string} email - The email of the user to be created.
  * @returns {Object} An object containing the registration token and registration status.
  */
-export function createSuperAdmin(email) {
-  // Send a POST request to create a super admin
+export function createUser(endpoint, email) {
   const resp = session.post(
-    `/api/v1/super_admins`,
+    endpoint,
     JSON.stringify({
       email: email,
     })
@@ -17,16 +17,36 @@ export function createSuperAdmin(email) {
 
   // Ensure that the user creation was successful (HTTP status 201)
   expect(resp.status, "User creation status").to.equal(201);
+
   // Ensure that the response body is a valid JSON
-  expect(resp, "User creation valid json response").to.have.validJsonBody();
+  expect(resp, "User creation valid JSON response").to.have.validJsonBody();
 
   // Ensure that the registration token is a string
   const registerationToken = resp.json("data.registrationToken");
   expect(registerationToken, "Registration token").to.be.a("string");
 
-  //  Ensure that the registration status is a boolean
+  // Ensure that the registration status is a boolean
   const isRegistered = resp.json("data.isRegistered");
   expect(isRegistered, "isRegistered").to.be.a("boolean");
 
   return { registerationToken, isRegistered };
+}
+
+/**
+ * Creates a super admin with the provided email.
+ * @param {string} email - The email of the super admin to be created.
+ * @returns {Object} An object containing the registration token and registration status.
+ */
+export function createSuperAdmin(email) {
+  return createUser("/api/v1/super_admins", email);
+}
+
+/**
+ * Creates an organization admin with the provided email and organization ID.
+ * @param {string} email - The email of the organization admin to be created.
+ * @param {string} organizationId - The ID of the organization.
+ * @returns {Object} An object containing the registration token and registration status.
+ */
+export function createOrganizationAdmin(email, organizationId) {
+  return createUser(`/api/v1/organizations/${organizationId}/admins`, email);
 }
