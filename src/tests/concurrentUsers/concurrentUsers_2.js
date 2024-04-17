89bae60334adfd550a559c2d787e4bf06219ff44
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-04-17 14:16:12 -0600

    refactor: remove repeated code

--- src/tests/concurrentUsers/concurrentUsers_2.js
+++ src/tests/concurrentUsers/concurrentUsers_2.js
@@ -1,8 +1,5 @@
-import { superAdminOperations } from '../../roles/superAdmin.js';
-import { describe } from "../../util/chaiExpect.js";
-import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
-import { createSuperAdmin } from '../../models/user.js';
-import { confirmRegister, login } from '../../util/authentication.js';
+import { superAdminOperations } from "../../roles/superAdmin.js";
+import { commonTestSetup } from "../../common/testSetup.js";
 
 export const options = {
   vus: 10,
@@ -14,40 +11,10 @@ export const options = {
   },
 }
 
-const SUPER_ADMIN_INFO = {
-  email: `superadmin${randomIntBetween(1, 100000)}@test.com`,
-  password: "verySecure",
-  firstName: "Super",
-  middleName: "Admin",
-  lastName: "Test",
-};
+export function setup() {
+  return commonTestSetup();
+}
 
-export default function () {
-  let authToken = "";
-
-  // Set up the super admin user
-  describe(`Setup - Create and authenticate super admin ${SUPER_ADMIN_INFO.email}`, () => {
-    // Create super admin if not already registered
-    const { registerationToken, isRegistered } = createSuperAdmin(
-      null,
-      SUPER_ADMIN_INFO.email
-    );
-    if (!isRegistered) {
-      describe("Confrim registeration for super admin", () => {
-        confirmRegister(
-          registerationToken,
-          SUPER_ADMIN_INFO.firstName,
-          SUPER_ADMIN_INFO.middleName,
-          SUPER_ADMIN_INFO.lastName,
-          SUPER_ADMIN_INFO.password
-        );
-      });
-    }
-
-    // Login the super admin user
-    describe("Login Super Admin", () => {
-      authToken = login(SUPER_ADMIN_INFO.email, SUPER_ADMIN_INFO.password);
-    });
-  });
-  superAdminOperations(authToken);
+export default function (superAdminAuthToken) {
+  superAdminOperations(superAdminAuthToken);
 }
